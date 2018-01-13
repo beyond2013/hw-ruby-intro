f7ccdcc7276ea7d092a1e4670c86b19f97b971d4
beyond2013 <imran.shahjee@gmail.com>
2018-01-13 16:05:02 +0700

    removed unnecessary code

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -1,6 +1,5 @@
 # part 1
-def sum(ary)
-	array = Array.new(ary)
+def sum(array)
 	result=0
 	if !array.empty? 
 	 return array.sum
@@ -9,17 +8,15 @@ def sum(ary)
 	end
 end
 
-def max_2_sum(aray)
-	array = Array.new(aray)
+def max_2_sum(array)
 	return 0 if array.empty?
 	return array[0] if array.length == 1
-	array = array.sort
+	array.sort!
 	max2sum = array[array.length-2] + array[array.length-1]
 	return max2sum
 end
 
-def sum_to_n?(aray, n)
-	array = Array.new(aray)
+def sum_to_n?(array, n)
 	return false if array.empty?
 	perm = array.permutation(2).to_a
 	result = false
@@ -36,13 +33,11 @@ def hello(name)
 	return "Hello, #{name}"
 end
 
-def starts_with_consonant?(s)
-	str = String.new(s)
+def starts_with_consonant?(str)
   return /\A[^aeiou\W]/i.match?(str)	
 end
 
-def binary_multiple_of_4?(s)
- str = String.new(s)
+def binary_multiple_of_4?(str)
  return false if str.empty?
  if /\A[0-1]*\z/.match?(str)
 	 return str.to_i(2) % 4 == 0
@@ -54,31 +49,15 @@ end
 #part 3
 class BookInStock
 
+	attr_accessor :isbn, :price 
+
 	def initialize(isbn, price)
-			if isbn.empty? or price <= 0
-				raise ArgumentError, "ISBN Cannot be emtpy, and price should be greater than zero"
-			else
-				@isbn = isbn
-				@price = price
-			end
+		if isbn.empty? or price <= 0
+			raise ArgumentError, "ISBN Cannot be emtpy, and price should be greater than zero"
+		else
+			@isbn, @price = isbn, price
+		end
 	end
-
-  def isbn=(isbn)
-		@isbn=isbn
-	end
-
-	def price=(price)
-		@price=price
-	end
-
-	def isbn
-		@isbn
-	end
-	
-	def price
-		@price
-	end
-
 
 	def price_as_string
 		return "$%0.2f" % [@price]
